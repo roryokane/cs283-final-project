bf0cf4676b68733a8ea31a7e5b90630c8022f4c7
Rory O’Kane <rory@roryokane.com>
2013-02-20 19:48:08 -0500

    add port option, move convert to new file
    add command-line option to set port, which involves parsing command-line options
    
    move convert function to new file
    
    add configuration settings to set port option easily

---
+++ lib/convert.rb
@@ -0,0 +1,5 @@
+# encoding: utf-8
+
+def convert_bc_to_c(better_c)
+	better_c.reverse
+end
--- lib/server.rb
+++ lib/server.rb
@@ -1,9 +1,32 @@
 # encoding: utf-8
 
 require 'sinatra'
+require 'optparse'
+require 'memoist'
+require_relative 'convert'
+require_relative 'config_loader'
 
-def convert_bc_to_c(better_c)
-	better_c.reverse
+extend Memoist
+CONFIG = ConfigLoader.new.config_for("server")
+
+
+def options
+	defaults = CONFIG['default options']
+	options = defaults
+	
+	OptionParser.new do |opt|
+		opt.banner = "Usage: server [OPTS]"
+		opt.on('-p', '--port N', Numeric, 'Port to listen on.', "Default: #{defaults[:port]}") { |p| options[:port] = p }
+		opt.parse!(ARGV)
+	end
+	
+	options
+end
+memoize :options
+
+
+configure do
+	set :port, options[:port]
 end
 
 get '/convert' do
