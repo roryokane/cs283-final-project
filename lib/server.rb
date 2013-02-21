e5ed46f08a1f732b9d401f800ca852ecc85a101b
Rory O’Kane <rory@roryokane.com>
2013-02-20 19:59:43 -0500

    remove port option code; Sinatra gives that by default

--- lib/server.rb
+++ lib/server.rb
@@ -1,33 +1,10 @@
 # encoding: utf-8
 
 require 'sinatra'
-require 'optparse'
-require 'memoist'
 require_relative 'convert'
 require_relative 'config_loader'
 
-extend Memoist
 CONFIG = ConfigLoader.new.config_for("server")
-
-
-def options
-	defaults = CONFIG['default options']
-	options = defaults
-	
-	OptionParser.new do |opt|
-		opt.banner = "Usage: server [OPTS]"
-		opt.on('-p', '--port N', Numeric, 'Port to listen on.', "Default: #{defaults[:port]}") { |p| options[:port] = p }
-		opt.parse!(ARGV)
-	end
-	
-	options
-end
-memoize :options
-
-
-configure do
-	set :port, options[:port]
-end
 
 # GET is appropriate because the result is idempotent; conversions always work the same way
 get '/convert' do
