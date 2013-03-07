2df1976219a18b98f6e862a958afb714ded1b9a8
Rory O’Kane <rory@roryokane.com>
2013-03-07 04:28:13 -0500

    fix style of expected C output for K&R cat

--- test/test-cases/k&r-cat-v2.c
+++ test/test-cases/k&r-cat-v2.c
@@ -6,10 +6,10 @@ main(int argc, char *argv[]) {
 	void filecopy(FILE *, FILE *);
 	char *prog = argv[0];  /* program name for errors */
 	
-	if (argc == 1 ) /* no args; copy standard input */
+	if (argc == 1 ) { /* no args; copy standard input */
 		filecopy(stdin, stdout);
-	else
-		while (--argc > 0)
+	} else {
+		while (--argc > 0) {
 			if ((fp = fopen(*++argv, "r")) == NULL) {
 				fprintf(stderr, "%s: can’t open %s\n",
 				        prog, *argv);
@@ -18,6 +18,8 @@ main(int argc, char *argv[]) {
 				filecopy(fp, stdout);
 				fclose(fp);
 			}
+		}
+	}
 	if (ferror(stdout)) {
 		fprintf(stderr, "%s: error writing stdout\n", prog);
 		exit(2);
