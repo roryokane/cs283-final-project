a442456bc1a7df27e0c1b483eb99caf155966cb2
Rory O’Kane <rory@roryokane.com>
2013-03-06 19:29:09 -0500

    implement hacky but working compilation
    I found out that the due date for the project was earlier than I thought it was. I was forced to code this in a hurry. It works for good input, but will fail on more complicated programs – for instance, programs with multiline comments, with multiline statements, with preprocessing directives, and with spaces for indentation.

--- lib/converting.rb
+++ lib/converting.rb
@@ -1,5 +1,77 @@
 # encoding: utf-8
 
 def convert_bc_to_c(better_c)
+	dumb_implementation(better_c)
+end
+
+def test_implementation(better_c)
 	better_c.chomp.reverse
+end
+
+def dumb_implementation(better_c)
+	lines = better_c.split("\n")
+	
+	# add parens around conditionals
+	original_lines = lines.dup
+	original_lines.each_with_index do |line, index|
+		paren_keywords = ["if", "while", "for"]
+		paren_keywords.each do |keyword|
+			if line.match(/\s*#{keyword}/)
+				lines[index] = lines[index].sub("#{keyword} ", keyword + " (")
+				lines[index] += ")"
+			end
+		end
+	end
+	
+	# add {} from indents
+	# only works with tab indents right now
+	indent_level = 0
+	original_lines = lines.dup
+	original_lines.each_with_index do |line, index|
+		line_indent_level = (line.match(/\t*/)[0]).size
+		if line_indent_level > indent_level
+			lines[index - 1] = lines[index - 1] + " {"
+		elsif line_indent_level < indent_level
+			indents = "\t" * line_indent_level
+			lines = lines.insert(index, indents + "}")
+		end
+		indent_level = line_indent_level
+	end
+	
+	# add semicolons
+	lines = lines.map do |line|
+		line_needs_semicolon = true
+		
+		starting_cancels = ["//"]
+		ending_cancels = ["{", "}", ";"]
+		regex_cancels = [/^\s*$/]
+		
+		starting_cancels.each do |start|
+			if line.start_with?(start)
+				line_needs_semicolon = false
+			end
+		end
+		ending_cancels.each do |an_end|
+			if line.end_with?(an_end)
+				line_needs_semicolon = false
+			end
+		end
+		regex_cancels.each do |regex|
+			if line.match(regex)
+				line_needs_semicolon = false
+			end
+		end
+		
+		if line_needs_semicolon
+			line + ";"
+		else
+			line
+		end
+	end
+	
+	return lines.join("\n")
+end
+
+def parsing_implementation(better_c)
+	# not written yet
 end
