a42bbcba4d27c7db80b7624199ca01c8e3bbcd5c
Rory O’Kane <rory@roryokane.com>
2013-07-04 00:05:37 -0400

    fix desired lots-of-indentation.c
    was missing semicolons

--- test/test-cases/lots-of-indentation.c
+++ test/test-cases/lots-of-indentation.c
@@ -1,30 +1,30 @@
 int count = 0;
 while (count % 3 == 2) {
-	int i
+	int i;
 	for (i=0; i<5; i++) {
-		count = foo(count)
+		count = foo(count);
 		if count == 0 {
-			printf("count is zero")
+			printf("count is zero");
 		}
 	for (i=count; i<count*2; i+=3) {
 	}
-	printf("calculated")
+	printf("calculated");
 }
-result = i
+result = i;
 
 if (result) {
 	
 	while (! result) {
-		bar(result)
+		bar(result);
 	}
 	if (result == 2) {
-		gall(31)
+		gall(31);
 	}
 	else if (result > 0) {
-		gall(559)
+		gall(559);
 	}
 	if (do_check()) {
-		printf("it's okay")
+		printf("it's okay");
 	}
 		
 	
